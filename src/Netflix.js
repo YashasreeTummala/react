3c51d4c757e45bd5eb9ad60e0bf8c4ce4790e4a1
YashasreeTummala <yashasreeaditya@gmail.com>
2023-02-18 17:57:37 +0530

    [Yash] Add. counter State Management via Virtual DOM

--- src/Netflix.js
+++ src/Netflix.js
@@ -1,12 +1,26 @@
-import './Netflix.css'
+import './Netflix.css';
+import {useEffect , useState} from 'react';
 
-function Netflix() {
+export default function Netflix() {
+    const [count , setCount] = useState(0);
+    useEffect(() => {
+            let x = setInterval(() => {
+                setCount(count => count + 1);
+            }, 1000);
+        }, [])
+
     return (
         <div className="bgImg">
+
             <div className="navbar">
                 <h1 className="logo"> NETFLIX </h1>
                 <button className="sign-btn"> Sign up! </button>
             </div>
+
+            <div>
+                <h1 className="counter"> Hits : {count} </h1>
+            </div>
+
             <div className="content">
                 <h1> Umlimited Movies, TV <br /> Shows and more. </h1>
                 <h3> Watch anywhere! Cancel anytime! </h3>
@@ -18,5 +32,3 @@ function Netflix() {
         </div>
     );
 }
-
-export default Netflix;
