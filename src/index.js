4a80dee1f1649f75327f27a82656dbc351a25b9e
YashasreeTummala <yashasreeaditya@gmail.com>
2023-02-18 19:18:09 +0530

    [Yash] Modify. Netflix page-1 using bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Netflix from './Netflix';
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
