fd2792685e9fa073d84d7101611d3f8aeeff00d7
YashasreeTummala <yashasreeaditya@gmail.com>
2023-02-22 11:22:41 +0530

    [Yash] types of components - functional & class

---
+++ src/modules/react_components/ClassCompo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class ClassCompo extends Component {
+  render() {
+    return (
+      <div>Class Component</div>
+    )
+  }
+}
+
+export default ClassCompo
---
+++ src/modules/react_components/FuncCompo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FuncCompo() {
+  return (
+    <div>Functional Component</div>
+  )
+}
+
+export default FuncCompo
