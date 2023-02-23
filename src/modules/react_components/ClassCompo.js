6ad9aabba15f726ef54b75e65eddbf730285e155
YashasreeTummala <yashasreeaditya@gmail.com>
2023-02-23 20:36:05 +0530

    [Yash | Pavan] Add. set state in class level to update the count

--- src/modules/react_components/ClassCompo.js
+++ src/modules/react_components/ClassCompo.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import './Compo.css'
-import Button from './Button.js'
+import Button1 from './Button1.js'
 
 export class ClassCompo extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      count :10
+    }
+  }
+
   render() {
     return (
       <div className="clas">
         <h2>Class Component</h2>
-        <Button name="Class Button"> </Button>
+        <Button1 name="Class Button"> </Button1>
+        <h2> {this.state.count} </h2>
+        <button onClick={() => this.setState({count : this.state.count + 10})}> Update Count </button>
+        
       </div>
     )
   }
